0f3caad37b017671f095437f96fc824dbfbdafa6
Julien Villemejane <julien.villemejane@institutoptique.fr>
2025-01-21 15:15:46 +0100

    UPdate DIgital Interface / Modifying subjets

--- InterfacageNumerique/bloc_rayonnement/codes/power_led.cpp
+++ InterfacageNumerique/bloc_rayonnement/codes/power_led.cpp
@@ -17,7 +17,6 @@
  *      Based on Mbed OS 6 example : mbed-os-example-blinky-baremetal
  */
 
-////////  ATTENTION !!!!   A TESTER AVEC LED FONCTIONNELLE !!
 
 #include "mbed.h"
 
@@ -26,24 +25,33 @@ DigitalOut cs_led(PB_5);
 
 void init_LED(int val){
     cs_led = 1;
-    my_spi.format(8,3);          
-    my_spi.frequency(100000);
+    my_spi.format(8,0);          
+    my_spi.frequency(250000);
     thread_sleep_for(100);
-
     // Send value of the potentiometer wiper.
-    cs_led = 0;
-    uint8_t address = 0;    // Wiper control register 
+    cs_led = 0; 
+    int cfg_value = 0b0100001111;
+    uint8_t address = 0x04; // Config control register 
     uint8_t command = 0;    // Write command   
-    uint8_t transferByte = (0 << 4) | ((0 << 2) | 0);
+    uint8_t transferByte = (address << 4) | (command << 2) | 0b01;
     my_spi.write(transferByte);
-    my_spi.write(val);
+    my_spi.write(cfg_value);
     cs_led = 1;
+    thread_sleep_for(10);
+    cs_led = 0;
+    transferByte = (0 << 4) | (0 << 2) | 0;
+    my_spi.write(transferByte);
+    my_spi.write(255-val);
+    cs_led = 1;
+    thread_sleep_for(10);
 }
 
 
 int main()
 {    
-    init_LED(100);
+    init_LED(10);
     
-    while (true){}
+    while (true){
+        thread_sleep_for(100);
+    }
 }
