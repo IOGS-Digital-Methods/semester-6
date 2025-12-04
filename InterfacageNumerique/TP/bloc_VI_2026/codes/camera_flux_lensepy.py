1db075b5b059284de74d47884a285a3f25598c0f
Julien Villemejane <julien.villemejane@institutoptique.fr>
2025-12-04 14:48:07 +0100

    Update Int Num / VI Labs

--- InterfacageNumerique/TP/bloc_VI_2026/codes/camera_flux_lensepy.py
+++ InterfacageNumerique/TP/bloc_VI_2026/codes/camera_flux_lensepy.py
@@ -10,89 +10,76 @@ import numpy as np
 from lensepy.utils import get_screen_size
 from lensepy.modules.basler.basler_models import init_first_camera
 
-COLOR_MODE = "Mono12"  # "BGR8" / "Mono12"
-    
+COLOR_MODE = "BGR8"  # "BGR8" / "Mono12"
+GAUSS_SIZE = (7, 7)         # Taille du flou gaussien
+GAUSS_SIGMA = 1.9
 
 def main():
     # --- 
     height, width = get_screen_size()
     mode = 'z'
     
-    cam2 = init_first_camera()
-    print(cam2)
+    camera = init_first_camera('./camera_vi.ini')
+    if camera is None:
+        cv2.destroyAllWindows()
+        return
     
-    # --- Initialisation de la caméra ---
-    camera = pylon.InstantCamera(pylon.TlFactory.GetInstance().CreateFirstDevice())
-    camera.Open()
-
-    converter = pylon.ImageFormatConverter()
-    if COLOR_MODE == 'BGR8':
-        # BGR8
-        camera.PixelFormat = "BGR8"     # OpenCV displays in BGR
-        converter.OutputPixelFormat = pylon.PixelType_BGR8packed
-        converter.OutputBitAlignment = pylon.OutputBitAlignment_MsbAligned
-    elif COLOR_MODE == 'Mono12':
-        # Mono 12
-        camera.PixelFormat = "Mono12"
-        converter.OutputPixelFormat = pylon.PixelType_Mono16  # On convertit vers 16 bits
-        converter.OutputBitAlignment = pylon.OutputBitAlignment_LsbAligned
+    camera.set_parameter('PixelFormat', COLOR_MODE)
+    camera.open()
+    camera.camera_acquiring = True
     
-    camera.StartGrabbing(pylon.GrabStrategy_LatestImageOnly)
     print("Appuyez sur 'q' pour quitter")
-
+    
     # --- Boucle d'acquisition ---
-    while camera.IsGrabbing():
-        grab = camera.RetrieveResult(5000, pylon.TimeoutHandling_ThrowException)
-
-        if grab.GrabSucceeded():
-            # --- Get Image ---
-            img = converter.Convert(grab)
-            frame_raw = img.GetArray()
-            
-            
-            # --- Processing ---
-            if mode == 'a': # Canny
-                if COLOR_MODE == 'Mono12':
-                    # Conversion 12 bits → 8 bits
-                    frame8 = (frame_raw / 16).astype(np.uint8)
-                else:
-                    frame8 = frame_raw
-                # Canny
-                final_output = cv2.Canny(frame8, 50, 150)
+    while camera.camera_acquiring:
+        # --- Get Image ---
+        frame_raw = camera.get_image()
+        # --- Processing ---
+        if mode == 'a': # Canny
+            if COLOR_MODE == 'Mono12':
+                # Conversion 12 bits → 8 bits
+                frame8 = (frame_raw / 16).astype(np.uint8)
             else:
-                if COLOR_MODE == 'Mono12':
-                    # Conversion 12 bits → 8 bits
-                    frame8 = (frame_raw / 16).astype(np.uint8)
-                else:
-                    frame8 = frame_raw
-                final_output = frame8
-            # Redimensionnement
-            display = cv2.resize(final_output, (width//2, height//2))
-            cv2.imshow("Flux Basler a2A1920", display)
+                frame8 = frame_raw
+            
+            blur_frame8 = cv2.GaussianBlur(frame8, GAUSS_SIZE, GAUSS_SIGMA)
+            # Canny
+            final_output = cv2.Canny(blur_frame8, 50, 150)
+        else:
+            if COLOR_MODE == 'Mono12':
+                # Conversion 12 bits → 8 bits
+                frame8 = (frame_raw / 16).astype(np.uint8)
+            else:
+                frame8 = frame_raw
+            final_output = frame8
+        # Redimensionnement
+        display = cv2.resize(final_output, (width//2, height//2))
+        cv2.imshow("Flux Basler a2A1920", display)
 
-            # --- Mode management ---
-            key = cv2.waitKey(1) & 0xFF
-            if key == ord('q'):
-                break
-            elif key == ord('a'):
-                mode = 'a'
-            elif key == ord('z'):
-                mode = 'z'
-                                
-            # --- Histogramme ---
-            hist = cv2.calcHist([frame8],[0],None,[256],[0,256])
-            hist_norm = cv2.normalize(hist, None, 0, 200, cv2.NORM_MINMAX)
-            hist_img = np.full((200, 256), 255, dtype=np.uint8)
+        # --- Mode management ---
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord('q'):
+            camera.camera_acquiring = False
+            camera.close()
+        elif key == ord('a'):
+            mode = 'a'
+        elif key == ord('z'):
+            mode = 'z'
+                            
+        # --- Histogramme ---
+        hist = cv2.calcHist([frame8],[0],None,[256],[0,256])
+        hist_norm = cv2.normalize(hist, None, 0, 200, cv2.NORM_MINMAX)
+        hist_img = np.full((200, 256), 255, dtype=np.uint8)
 
-            for x in range(256):
-                cv2.line(hist_img, (x, 200), (x, 200 - int(hist_norm[x])), 0, 1)
+        for x in range(256):
+            value = int(hist_norm[x].item())   # ✔ Conversion safe, plus de warning
+            cv2.line(hist_img, (x, 200), (x, 200 - value), 0, 1)
 
-            cv2.imshow("Histogramme", hist_img)
+        cv2.imshow("Histogramme", hist_img)
 
-        grab.Release()
+        #grab.Release()
 
-    camera.StopGrabbing()
-    camera.Close()
+    camera.close()
     cv2.destroyAllWindows()
 
 if __name__ == "__main__":
